3efce386d20abb744c942720fb7d39e0d7e4e093
Dean Andrew Hidas <dhidas@bnl.gov>
2021-03-22 14:00:43 -0400

    testing

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = 'radiamodels',
     version = '0.1',
     packages = ['radiamodels'],
-    package_data = {'radiamodels/data': ['data/*.dat', 'data/*.txt']},
-    data_files=[('radiamodels/data', ['data/PermendurNEOMAX.txt',])],
+    include_package_data=True,
+#    package_data = {'radiamodels/data': ['data/*.dat', 'data/*.txt']},
+#    data_files=[('radiamodels/data', ['data/PermendurNEOMAX.txt',])],
 )
