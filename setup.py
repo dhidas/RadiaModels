1f0a3ff554f97b342f40c6615152f4908b72febf
Dean Andrew Hidas <dhidas@bnl.gov>
2024-04-17 11:57:19 -0400

    update test models and version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'radiamodels',
-    version = '0.4',
+    version = '0.5',
     packages = ['radiamodels'],
     package_dir={'radiamodels': 'radiamodels'},
     package_data = {'radiamodels': ['data/*.dat', 'data/*.txt']},
